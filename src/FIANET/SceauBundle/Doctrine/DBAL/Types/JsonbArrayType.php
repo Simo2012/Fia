d2e9ef83c8dfef2f337defcc55a0a27e6aa314f9
Christophe Meneses <christophe.meneses@fia-net.com>
2015-08-24 12:19:00 +0000

    Doctrine : ajout du type jsonb
    git-svn-id: http://10.199.54.108/svn/Sceau/trunk@82 2f395b5d-8448-4617-aa85-2f7e1c74ebc6

---
+++ src/FIANET/SceauBundle/Doctrine/DBAL/Types/JsonbArrayType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace FIANET\SceauBundle\Doctrine\DBAL\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\JsonArrayType;
+
+class JsonbArrayType extends JsonArrayType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return 'JSONB';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'jsonb_array';
+    }
+}
