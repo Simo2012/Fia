c8cfd08f64bb4ac6b7201f212f3e801ad7bc5048
Christophe Meneses <christophe.meneses@fia-net.com>
2015-09-09 14:07:12 +0000

    Config logs + ajout logs pour la validation des flux
    git-svn-id: http://10.199.54.108/svn/Sceau/trunk@97 2f395b5d-8448-4617-aa85-2f7e1c74ebc6

--- src/SceauBundle/Command/Cron/ValidationFluxCommand.php
+++ src/SceauBundle/Command/Cron/ValidationFluxCommand.php
@@ -3,6 +3,7 @@ namespace SceauBundle\Command\Cron;
 
 use Doctrine\Common\Persistence\ObjectManager;
 use Exception;
+use Psr\Log\LoggerInterface;
 use SceauBundle\Exception\FluxException;
 use SceauBundle\Service\GestionFlux;
 use SceauBundle\Service\GestionQuestionnaire;
@@ -14,16 +15,22 @@ use Symfony\Component\Console\Output\OutputInterface;
 class ValidationFluxCommand extends Command
 {
     private $em;
-    private $gf;
-    private $gq;
+    private $gestionFlux;
+    private $gestionQuest;
+    private $logger;
 
-    public function __construct(ObjectManager $em, GestionFlux $gf, GestionQuestionnaire $gq)
-    {
+    public function __construct(
+        ObjectManager $em,
+        GestionFlux $gestionFlux,
+        GestionQuestionnaire $gestionQuest,
+        LoggerInterface $logger
+    ) {
         parent::__construct();
 
         $this->em = $em;
-        $this->gf = $gf;
-        $this->gq = $gq;
+        $this->gestionFlux = $gestionFlux;
+        $this->gestionQuest = $gestionQuest;
+        $this->logger = $logger;
     }
 
     protected function configure()
@@ -35,7 +42,7 @@ class ValidationFluxCommand extends Command
             ->setHelp(<<<EOT
 <info>%command.name%</info> permet de valider ou non les flux XML des marchands et génère un questionnaire pour chaque flux valide.
 
-Il est obligatoire de preciser le nombre maximum de flux que la commande doit traiter.
+Il est obligatoire de préciser le nombre maximum de flux que la commande doit traiter.
 
 Exemple d'utilisation :
 
@@ -49,54 +56,80 @@ EOT
      *
      * On récupère dans un premier temps uniquement les identifiants des flux car on doit faire ensuite un update sur
      * ces flux. Si on récupère directement les objets Flux grâce à un paginator, l'update "casse" le paginator.
+     *
+     * En cas d'erreur non attendues (catch d'une Exception), on remet le flux dans son statut initial pour qu'il soit
+     * de nouveau traiter au prochain lancement de la commande.
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $nbMaxFlux = $input->getArgument('nbMaxFlux');
+        $this->logger->info("DEBUT de la validation de $nbMaxFlux flux");
 
-        if (!is_numeric($nbMaxFlux)) {
-            $output->writeln('<error>Le nombre maximum de flux doit etre un entier.</error>');
+        if (is_numeric($nbMaxFlux)) {
+            $listeIdFlux = $this->em->getRepository('SceauBundle:Flux')->listeIdFluxNonTraites($nbMaxFlux);
 
-            return 1;
-        }
+            if (!empty($listeIdFlux)) {
+                $fluxStatutATraiter = $this->em->getRepository('SceauBundle:FluxStatut')->aTraiter();
+                $fluxStatutValide = $this->em->getRepository('SceauBundle:FluxStatut')->traiteEtValide();
+                $fluxStatutNonValide = $this->em->getRepository('SceauBundle:FluxStatut')->traiteEtInvalide();
 
-        $listeIdFlux = $this->em->getRepository('SceauBundle:Flux')->listeIdFluxNonTraites($nbMaxFlux);
+                $this->em->getRepository('SceauBundle:Flux')->updateEnCoursDeTraitement($listeIdFlux);
+                $fluxNonTraites = $this->em->getRepository('SceauBundle:Flux')->listeFluxParId($listeIdFlux);
 
-        if (!empty($listeIdFlux)) {
-            $fluxStatutATraiter = $this->em->getRepository('SceauBundle:FluxStatut')->aTraiter();
-            $fluxStatutValide = $this->em->getRepository('SceauBundle:FluxStatut')->traiteEtValide();
-            $fluxStatutNonValide = $this->em->getRepository('SceauBundle:FluxStatut')->traiteEtInvalide();
+                foreach ($fluxNonTraites as $fluxNonTraite) {
+                    try {
+                        $this->gestionFlux->validerContenu($fluxNonTraite);
+                        $this->gestionQuest->genererQuestionnaireViaFlux($fluxNonTraite);
 
-            $this->em->getRepository('SceauBundle:Flux')->updateEnCoursDeTraitement($listeIdFlux);
-            $fluxNonTraites = $this->em->getRepository('SceauBundle:Flux')->listeFluxParId($listeIdFlux);
+                        $fluxNonTraite->setFluxStatut($fluxStatutValide);
 
-            foreach ($fluxNonTraites as $fluxNonTraite) {
-                try {
-                    $this->gf->validerContenu($fluxNonTraite);
-                    $this->gq->genererQuestionnaireViaFlux($fluxNonTraite);
+                        $this->logger->info(
+                            sprintf(
+                                'Le flux %d du site %d est valide',
+                                $fluxNonTraite->getId(),
+                                $fluxNonTraite->getSite()->getId()
+                            )
+                        );
 
-                    $fluxNonTraite->setFluxStatut($fluxStatutValide);
+                    } catch (FluxException $e) {
+                        $fluxNonTraite->setFluxStatut($fluxStatutNonValide);
+                        $fluxNonTraite->setLibelleErreur($e->getMessage());
 
-                } catch (FluxException $e) {
-                    $fluxNonTraite->setFluxStatut($fluxStatutNonValide);
-                    $fluxNonTraite->setLibelleErreur($e->getMessage());
+                        $this->logger->warning(
+                            sprintf(
+                                'Le flux %d du site %d n\'est pas valide : %s',
+                                $fluxNonTraite->getId(),
+                                $fluxNonTraite->getSite()->getId(),
+                                $e->getMessage()
+                            )
+                        );
 
-                } catch (Exception $e) {
-                    $fluxNonTraite->setFluxStatut($fluxStatutATraiter);
-                    $output->writeln('<error>La validation du flux ' . $fluxNonTraite->getId() .
-                        ' a échoué : ' . $e->getMessage() . '</error>');
+                    } catch (Exception $e) {
+                        $fluxNonTraite->setFluxStatut($fluxStatutATraiter);
+
+                        $this->logger->error(
+                            sprintf(
+                                'Erreur lors de la validation du flux %d du site %d : %s',
+                                $fluxNonTraite->getId(),
+                                $fluxNonTraite->getSite()->getId(),
+                                $e->getMessage()
+                            )
+                        );
+                    }
                 }
+                $this->em->flush();
+
+                $output->writeln('<info>La validation est terminée !</info>');
+
+            } else {
+                $output->writeln('<info>Il n\'y a aucun flux à traiter.</info>');
             }
-            $this->em->flush();
-
-            $output->writeln('<info>La validation est terminée !</info>');
-
-            return 0;
 
         } else {
-            $output->writeln('<info>Il n\'y a aucun flux à traiter.</info>');
-
-            return 0;
+            $output->writeln('<error>Le nombre maximum de flux doit etre un entier.</error>');
+            $this->logger->error("Le nombre maximum de flux passé à la commande est incorrect");
         }
+
+        $this->logger->info("FIN de la validation des flux");
     }
 }
