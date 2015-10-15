6f389e21560a7a178bfc6b82e3dec2e38015a3de
ragoupady sagadevin <ragoupadysagadevin@MacBook-Air-de-ragoupady.local>
2015-10-15 12:15:38 +0200

    Mise en place de la récupération des article de presse

--- src/SceauBundle/Controller/BackOffice/ArticlePresseController.php
+++ src/SceauBundle/Controller/BackOffice/ArticlePresseController.php
@@ -6,25 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Pagerfanta\Pagerfanta;
 
 class ArticlePresseController extends Controller
 {
     /**
      * List all published Articles.
      *
-     * @Route("/articles/{page}", name="articles")
+     * @Route("/articles", name="articles")
      * @Method("GET")
      */
     public function indexAction(Request $request, $page)
     {
         $articlePresseRepo = $this->get('sceau.repository.article.presse');
-        $adapter = $articlePresseRepo->getAllArticlePresse();
-        $pagerfanta = new Pagerfanta($adapter);
-        $pagerfanta->setMaxPerPage($this->getParameter('pagination_size'))
-        ->setCurrentPage($page)
-        ;
-        var_dump("ragou");die();
-        //return $this->render('SceauBundle:BackOffice:index.html.twig', array('tabArticlePresse' => $pagerfanta));
+        $articlePresses = $articlePresseRepo->findBy(array(), array('date' => 'ASC'));
+
+        return $this->render('SceauBundle:Admin/Articles:index.html.twig', array('articlePresses' => $articlePresses));
+
     }
 }
--- src/SceauBundle/Entity/Repository/ArticlePresseRepository.php
+++ src/SceauBundle/Entity/Repository/ArticlePresseRepository.php
@@ -5,6 +5,7 @@ namespace SceauBundle\Entity\Repository;
 use Doctrine\ORM\EntityRepository;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 
+
 /**
  * ArticlePresseRepository
  *
@@ -16,11 +17,14 @@ class ArticlePresseRepository extends EntityRepository
     public function getAllArticlePresse()
     {
         $queryBuilder = $this->createQueryBuilder('a');
-        $queryBuilder->where('a.published = true');
+        $queryBuilder->where('a.published = true')
+                     ->orderBy('a.date', 'DESC')
+        ;
 
         return $adapter = new DoctrineORMAdapter($queryBuilder);
 
     }
+
 
 
 
