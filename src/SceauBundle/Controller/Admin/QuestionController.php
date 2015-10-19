bcdf8103e46b61e0d45113ebb753d53ac3809f66
Hugo Piso <hpiso@pm-00037.local>
2015-10-19 17:17:07 +0200

    Gestion du menu sur le BO

---
+++ src/SceauBundle/Controller/Admin/QuestionController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SceauBundle\Controller\Admin;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * LitigeController controller.
+ *
+ * @Route("/questions")
+ */
+class QuestionController extends Controller
+{
+
+    /**
+     * Litiges.
+     *
+     * @Route("/", name="questions")
+     * @Method("GET")
+     * @Template("SceauBundle:Admin/Questions:index.html.twig")
+     */
+    public function indexAction(Request $request)
+    {
+
+    }
+
+}
