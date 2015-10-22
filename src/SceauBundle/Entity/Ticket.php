728dcce8a8c9943779ae3a585966d5eb7b182eab
aageorges <aageorges@clever-age.com>
2015-10-22 17:54:17 +0200

    icescrum#14 - Partie Contact - Fix global issu with js - Fix form todo: - contact form

--- src/SceauBundle/Entity/Ticket.php
+++ src/SceauBundle/Entity/Ticket.php
@@ -12,6 +12,57 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Ticket
 {
+    // ACTOR
+    const INDIVIDUAL = 1;
+    const PROFESSIONAL = 2;
+
+    public static $ACTORS = [
+        self::INDIVIDUAL   => 'Un particulier',
+        self::PROFESSIONAL => 'Un professionnel',
+    ];
+
+    // TYPE INDIVIDUAL
+    const COMMAND_CANCEL                = 11;
+    const COMMAND_AWAIT                 = 12;
+    const WEBSITE_TECHNICAL_PROBLEM     = 13;
+    const MEMBER_AREA_TECHNICAL_PROBLEM = 14;
+    const LITIGATION                    = 15;
+    const JOB_OFFER                     = 16;
+    const PHISHING                      = 17;
+
+    // TYPE PROFESSIONAL
+    const REPORTER   = 21;
+    const E_MERCHANT = 22;
+    const WEB_AGENCY = 23;
+
+    public static $TYPES = [
+        self::INDIVIDUAL => [
+            self::COMMAND_CANCEL                => 'Une commande annulée par un e-commerçant',
+            self::COMMAND_AWAIT                 => 'Une commande en attente de pièces justificatives',
+            self::WEBSITE_TECHNICAL_PROBLEM     => 'Un problème technique sur le site FIA-NET.com',
+            self::MEMBER_AREA_TECHNICAL_PROBLEM => 'Un problème technique dans votre espace membre FIA-NET',
+            self::LITIGATION                    => 'La déclaration d\'un litige avec un e-commerçant sur FIA-NET.com',
+            self::JOB_OFFER                     => 'Les offres d\'emplois disponibles',
+            self::PHISHING                      => 'Une tentative de phishing concernant FIA-NET',
+        ],
+        self::PROFESSIONAL => [
+            self::REPORTER   => 'Un journaliste',
+            self::E_MERCHANT => 'Un e-commerçant souhaitant être informé sur nos offres',
+            self::WEB_AGENCY => 'Une web agency souhaitant devenir partenaire du Sceau de Confiance FIA-NET',
+        ],
+    ];
+
+    public static $TYPES_TEMPLATE = [
+        self::COMMAND_CANCEL => 'SceauBundle:Site\Contact:command.html.twig',
+        self::COMMAND_AWAIT  => 'SceauBundle:Site\Contact:command.html.twig',
+        self::LITIGATION     => 'SceauBundle:Site\Contact:litigation.html.twig',
+        self::JOB_OFFER      => 'SceauBundle:Site\Contact:job_offer.html.twig',
+        self::PHISHING       => 'SceauBundle:Site\Contact:phishing.html.twig',
+        self::REPORTER       => 'SceauBundle:Site\Contact:reporter.html.twig',
+        self::E_MERCHANT     => 'SceauBundle:Site\Contact:e_merchant.html.twig',
+        self::WEB_AGENCY     => 'SceauBundle:Site\Contact:web_agency.html.twig',
+    ];
+
     /**
      * @var integer
      *
@@ -42,16 +93,9 @@ class Ticket
      */
     private $etat;
 
-    /**
-     * @ORM\ManyToOne(targetEntity="SceauBundle\Entity\TicketType")
-     * @ORM\JoinColumn(name="type_id", referencedColumnName="id", nullable=true)
-     */
+    private $actor;
+
     private $type;
-
-    private $acteur;
-    public function getActeur(){return $this->acteur;}
-    public function setActeur($acteur){$this->acteur = $acteur; return $this;}
-
 
     /**
      * Get id
@@ -135,27 +179,48 @@ class Ticket
         return $this->etat;
     }
 
-    /**
-     * Set type
-     *
-     * @param \SceauBundle\Entity\TicketType $type
-     *
-     * @return Ticket
-     */
-    public function setType(\SceauBundle\Entity\TicketType $type = null)
+
+    public function getActor()
     {
-        $this->type = $type;
+        return $this->actor;
+    }
+    public function getActorLabel()
+    {
+        return self::$ACTORS[$this->actor];
+    }
+    public function setActor($actor)
+    {
+        if (array_key_exists($actor, self::$ACTORS)) {
+            $this->actor = $actor;
+        }
 
         return $this;
     }
 
-    /**
-     * Get type
-     *
-     * @return \SceauBundle\Entity\TicketType
-     */
     public function getType()
     {
         return $this->type;
+    }
+    public function getTypeLabel()
+    {
+        return self::$TYPES[$this->type];
+    }
+    public function setType($type)
+    {
+        if ($this->actor) {
+            if (array_key_exists($type, self::$TYPES[$this->actor])) {
+                $this->type = $type;
+            }
+        }
+
+        return $this;
+    }
+    public static function getAvailableTypes($actor)
+    {
+        if ($actor) {
+            return self::$TYPES[$actor];
+        }
+
+        return [];
     }
 }
--- src/SceauBundle/Form/Type/Site/TicketQuestionType.php
+++ src/SceauBundle/Form/Type/Site/TicketQuestionType.php
@@ -3,8 +3,11 @@
 namespace SceauBundle\Form\Type\Site;
 
 use Doctrine\ORM\EntityRepository;
+use SceauBundle\Entity\Ticket;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use SceauBundle\Entity\TicketActeur;
 use SceauBundle\Entity\TicketType;
@@ -14,53 +17,65 @@ use Symfony\Component\Form\FormEvents;
 class TicketQuestionType extends AbstractType
 {
     /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
+     * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('acteur', 'entity', array(
-                'class'    => 'SceauBundle:TicketActeur',
-                'property' => 'libelle',
-                //'mapped'   => false,
-                'empty_value' => ".......",
-            ));
+            ->add('actor', 'choice', [
+                'placeholder'   => '-----------------------------',
+                'empty_data'    => null,
+                'label'         => 'form.ticket.acteur',
+                'choices'       => Ticket::$ACTORS,
+            ]);
         ;
+
+        $formModifier = function (FormInterface $form, $actor) {
+            $form->add('type', 'choice', [
+                'placeholder'   => '-----------------------------',
+                'empty_data'    => null,
+                'label'         => 'form.ticket.type',
+                'choices'       => Ticket::getAvailableTypes($actor),
+            ]);
+        };
 
         $builder->addEventListener(
             FormEvents::PRE_SET_DATA,
-            function (FormEvent $event) {
-                $form = $event->getForm();
-
+            function (FormEvent $event) use ($formModifier) {
+                $form   = $event->getForm();
+                /** @var \SceauBundle\Entity\Ticket $ticket */
                 $ticket = $event->getData();
 
-            if ($ticket) {
-                $form->add('type', 'entity', array(
-                    'class'    => 'SceauBundle:TicketType',
-                    'property' => 'libelle',
-                    'query_builder' => function(EntityRepository $er) use ($ticket) {
-                        $qb = $er->createQueryBuilder('tt');
-                        return $qb->where($qb->expr()->eq('tt.acteur', $ticket->getId()));
-                    }
-                ));
+                if ($ticket && $ticket->getActor()) {
+                    $formModifier($form, $ticket->getActor());
+                }
             }
+        );
+
+        $builder->get('actor')->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) use ($formModifier) {
+                $actor = $event->getForm()->getData();
+
+                if ($actor) {
+                    $formModifier($event->getForm()->getParent(), $actor);
+                }
             }
         );
     }
 
     /**
-     * @param OptionsResolverInterface $resolver
+     * {@inheritdoc}
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(array(
-            //'data_class' => 'SceauBundle\Entity\Ticket'
-        ));
+        $resolver->setDefaults([
+            'data_class' => 'SceauBundle\Entity\Ticket',
+        ]);
     }
 
     /**
-     * @return string
+     * {@inheritdoc}
      */
     public function getName()
     {
