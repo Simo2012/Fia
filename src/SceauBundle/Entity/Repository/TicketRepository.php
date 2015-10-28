78e5af15a8e3e6022c47c76352bc1222b3e828f2
Quentin Dupertuis <qdupertuis@clever-age.com>
2015-10-28 10:00:25 +0100

    ok pour merge with rag

--- src/SceauBundle/Entity/Repository/TicketRepository.php
+++ src/SceauBundle/Entity/Repository/TicketRepository.php
@@ -10,18 +10,15 @@ class TicketRepository extends EntityRepository
 
     public function getTicketsByParams($params)
     {
-    	var_dump('repo : ', $params);
         $qb = $this->createQueryBuilder('t')
         	->where('t.id is not null');
 
 
         if (isset($params['type']) && $params['type'] != '') {
-        	var_dump('set type : ', $params['type']);
         	$qb->andWhere($qb->expr()->eq('t.type.id', $params['type']));
         }
 
         if (isset($params['etat']) && $params['etat'] != '') {
-        	var_dump('set etat : ', $params['etat']);
         	$qb->andWhere('t.etat = :etat')
         	 	->setParameter('etat', $params['etat']);
         }
